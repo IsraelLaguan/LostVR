f8a7a5d726875815e823d276d4b2e5d94a60ebf4
Katu_brd <katuthings@gmail.com>
2017-09-06 13:54:26 +0200

    first step to animation of camera move

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 AFRAME.registerComponent('general-scene', {
+
+
         /**
          * Code within this function will be called when everything in <a-scene> is ready and loaded.
          */
         init: function () {
           var scene = document.querySelector('a-scene');
           var box = document.querySelector('#box');
+          var scd = document.querySelector('#box2');
           var sphere = document.querySelector('#sphere');
 
           console.log('box');
 
           box.addEventListener('click', function(){
-            var entity = scene.querySelector('a-entity');
-            entity.setAttribute('material', 'color', 'red');
+            var sph = document.querySelector('#sphere');
+            sph.setAttribute('geometry', {radius: 3});
           });
 
           sphere.addEventListener('click', function(){
-            this.setAttribute('geometry', {radius: 3});
+            box.setAttribute('material', 'color', '#66CC99');
           });
 
+          scd.addEventListener('click', function(){
+            var c = document.querySelector('#cam');
+            c.setAttribute('position', {x: 3, y: 3, z: 0.66});
+          
+          });
 
         }
       });
