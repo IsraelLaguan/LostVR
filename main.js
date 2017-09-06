4ba709e80fc4129118d9552e02559c12f359b06a
Katu_brd <katuthings@gmail.com>
2017-09-06 10:30:19 +0200

    work in progress

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+AFRAME.registerComponent('general-scene', {
+        /**
+         * Code within this function will be called when everything in <a-scene> is ready and loaded.
+         */
+        init: function () {
+          var scene = document.querySelector('a-scene');
+          var box = document.querySelector('a-entity');
+          console.log('box');
+          box.addEventListener('click', function(){
+            var entity = scene.querySelector('a-entity');
+            entity.setAttribute('material', 'color', 'red');
+          });
 
+
+        }
+      });
