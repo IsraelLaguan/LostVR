2c4a6b60dbcfa866a4d09d811718cb95ee3f9631
Katu_brd <katuthings@gmail.com>
2017-09-07 12:38:43 +0200

    creating enivironment

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-AFRAME.registerComponent('general-scene', {
+// AFRAME.registerComponent('general-scene', {
 
 
         /**
          * Code within this function will be called when everything in <a-scene> is ready and loaded.
          */
-        init: function () {
-          var scene = document.querySelector('a-scene');
-          var box = document.querySelector('#box');
-          var scd = document.querySelector('#box2');
-          var sphere = document.querySelector('#sphere');
-
-          console.log('box');
-
-          box.addEventListener('click', function(){
-            var sph = document.querySelector('#sphere');
-            sph.setAttribute('geometry', {radius: 3});
-          });
-
-          sphere.addEventListener('click', function(){
-            box.setAttribute('material', 'color', '#66CC99');
-          });
-
-          scd.addEventListener('click', function(){
-            var c = document.querySelector('#cam');
-            c.setAttribute('position', {x: 3, y: 3, z: 0.66});
-          
-          });
-
-        }
-      });
+        // init: function () {
+        //   var scene = document.querySelector('a-scene');
+        //   var box = document.querySelector('#box');
+        //   var scd = document.querySelector('#box2');
+        //   var sphere = document.querySelector('#sphere');
+        //   var c = document.querySelector('#cam');
+        //   var colorArray = [
+        //       '#22202B',
+        //       '#383745',
+        //       '#7D6962',
+        //       '#0F6177',
+        //       '#1BBC9B',
+        //   ]
+        //
+        //
+        //   console.log('box');
+        //
+        //   box.addEventListener('click', function(){
+        //     var c = document.querySelector('#cam');
+        //     c.setAttribute('position', {x: 8.5, y: 3, z: -12});
+        //   });
+        //
+        //   sphere.addEventListener('click', function(){
+        //     box.setAttribute('material', 'color', '#7D6962');
+        //   });
+        //
+        //   scd.addEventListener('click', function(){
+        //       var sph = document.querySelector('#sphere');
+        //       sph.setAttribute('geometry', {radius: 3});
+        //       box.setAttribute('geometry', {width: 3, height: 3, depth: 3});
+        //   });
+        //
+        // }
+      // });
