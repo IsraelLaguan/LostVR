2ea87803a4d18376850596ac4dad5f7004d9bbe2
Katu_brd <katuthings@gmail.com>
2017-09-08 12:07:26 +0200

    camera move animation

--- main.js
+++ main.js
@@ -1,40 +1,20 @@
-// AFRAME.registerComponent('general-scene', {
+   AFRAME.registerComponent('general-scene', {
+     init: function () {
+       var scene = document.querySelector('a-scene');
+       var first = document.querySelector('#one');
+       var moveOne = document.querySelector('#move1');
+
+      moveOne.emit('animationstart');
 
 
-        /**
-         * Code within this function will be called when everything in <a-scene> is ready and loaded.
-         */
-        // init: function () {
-        //   var scene = document.querySelector('a-scene');
-        //   var box = document.querySelector('#box');
-        //   var scd = document.querySelector('#box2');
-        //   var sphere = document.querySelector('#sphere');
-        //   var c = document.querySelector('#cam');
-        //   var colorArray = [
-        //       '#22202B',
-        //       '#383745',
-        //       '#7D6962',
-        //       '#0F6177',
-        //       '#1BBC9B',
-        //   ]
-        //
-        //
-        //   console.log('box');
-        //
-        //   box.addEventListener('click', function(){
-        //     var c = document.querySelector('#cam');
-        //     c.setAttribute('position', {x: 8.5, y: 3, z: -12});
-        //   });
-        //
-        //   sphere.addEventListener('click', function(){
-        //     box.setAttribute('material', 'color', '#7D6962');
-        //   });
-        //
-        //   scd.addEventListener('click', function(){
-        //       var sph = document.querySelector('#sphere');
-        //       sph.setAttribute('geometry', {radius: 3});
-        //       box.setAttribute('geometry', {width: 3, height: 3, depth: 3});
-        //   });
-        //
-        // }
-      // });
+       first.addEventListener('mouseenter', function(){
+         var c = document.querySelector('a-camera');
+         c.setAttribute('position', {X: -13, y: 7.5, z: 9.9});
+        //  c.setAttribute('animation', 'property: position; dur: 100; easing: easeInOutSine; to:-13 5.9 41.3;');
+
+       });
+
+
+
+     }
+   });
