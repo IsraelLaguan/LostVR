c01d65489a8151e3d5a3795d72c2530466424d46
Katu_brd <katuthings@gmail.com>
2017-09-08 15:52:37 +0200

    first scene animation

--- main.js
+++ main.js
@@ -3,13 +3,23 @@
        var scene = document.querySelector('a-scene');
        var first = document.querySelector('#one');
        var moveOne = document.querySelector('#move1');
+       var bushOne = document.querySelector('#right-white');
+       var bushTwo = document.querySelector('#left-white');
 
-      moveOne.emit('animationstart');
+       bushTwo.addEventListener('mouseenter', function(){
+         var sphere = document.querySelector('#firstAnSphere');
+         sphere.setAttribute('position', {x: -13.6, y: 4.6, z: 11.2});
+       });
 
+       bushOne.addEventListener('mouseenter', function(){
+         console.log('hey');
+         var plane = document.querySelector('#firstAnPlane');
+         plane.setAttribute('position', {x: -6.2, y: 4, z: 12.5});
+       });
 
        first.addEventListener('mouseenter', function(){
          var c = document.querySelector('a-camera');
-         c.setAttribute('position', {X: -13, y: 7.5, z: 9.9});
+         c.setAttribute('position', {x: 17, y: 5.9, z: 41.3});
         //  c.setAttribute('animation', 'property: position; dur: 100; easing: easeInOutSine; to:-13 5.9 41.3;');
 
        });
